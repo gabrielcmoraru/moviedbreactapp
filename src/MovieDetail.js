64514cb6e29e9224ff4958a01d59d0dc3c246d5a
Gabriel C Moraru <gabrielcmoraru@gmail.com>
2018-07-23 03:14:20 +0100

    First Real React App

---
+++ src/MovieDetail.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import Overdrive from 'react-overdrive';
+import { Poster } from './Movie';
+
+const POSTER_PATH = 'http://image.tmdb.org/t/p/w154';
+const BACKDROP_PATH = 'http://image.tmdb.org/t/p/w1280';
+
+
+class MovieDetail extends Component {
+  state = {
+    movie: {},
+  }
+
+  async componentDidMount() {
+    try {
+      const res = await fetch(`https://api.themoviedb.org/3/movie/${this.props.match.params.id}?api_key=a77c857565892e60c8a67dfc163683ab&language=en-UK`);
+      const movie = await res.json();
+      this.setState({
+        movie,
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  render() {
+    const { movie } = this.state;
+    return (
+      <MovieWrapper backdrop={`${BACKDROP_PATH}${movie.backdrop_path}`}>
+        <MovieInfo>
+          <Overdrive id={movie.id}>
+            <Poster src={`${POSTER_PATH}${movie.poster_path}`} alt={movie.title} />
+          </Overdrive>
+          <div>
+            <h1>
+              {this.state.movie.title}
+            </h1>
+            <h3>
+              {this.state.movie.release_date}
+            </h3>
+            <p>
+              {this.state.movie.overview}
+            </p>
+          </div>
+        </MovieInfo>
+      </MovieWrapper>
+    );
+  }
+}
+
+export default MovieDetail;
+
+
+const MovieWrapper = styled.div`
+  position: relative;
+  padding-top: 50vh;
+  background: url(${props => props.backdrop}) no-repeat;
+  background-size: cover;
+`;
+
+const MovieInfo = styled.div`
+  background: white;
+  text-align: left;
+  padding: 2rem 10%;
+  display: flex;
+  > div {
+    margin-left: 20px;
+  }
+  img {
+    position: relative;
+    top: -5rem;
+  }
+`;
---
+++ src/MoviesList.js
@@ -0,0 +1,39 @@
+import React, { PureComponent } from 'react';
+import styled from 'styled-components';
+import Movie from './Movie';
+
+class MoviesList extends PureComponent {
+  state = {
+    movies: [],
+  }
+
+  async componentDidMount() {
+    try {
+      const res = await fetch('https://api.themoviedb.org/3/discover/movie?api_key=a77c857565892e60c8a67dfc163683ab&language=en-US&sort_by=popularity.desc&page=1&timezone=America%2FNew_York&include_null_first_air_dates=false');
+      const movies = await res.json();
+      console.log(movies);
+      this.setState({
+        movies: movies.results,
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  render() {
+    return (
+      <MovieGrid>
+        {this.state.movies.map(movie => <Movie key={movie.id} movie={movie} />)}
+      </MovieGrid>
+    );
+  }
+}
+
+export default MoviesList;
+
+const MovieGrid = styled.div`
+  display: grid;
+  padding: 1rem;
+  grid-template-columns: repeat(6, 1fr);
+  grid-row-gap: 1rem;
+`;
